cde9af42184f3118ab19bbe143241f5b8589d0ba
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2026-04-24 17:51:15 +0530

    added controllers

---
+++ backend/controllers/todoController.js
@@ -0,0 +1,43 @@
+const Todo = require('../models/Todo');
+
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ createdAt: -1 });
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const todo = await Todo.create({ title, description });
+    res.status(201).json(todo);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!todo) return res.status(404).json({ error: 'Todo not found' });
+    res.json(todo);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.status(404).json({ error: 'Todo not found' });
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};

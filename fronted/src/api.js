ece1721276a57a4ce016e805f62f08d80ab93063
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2026-04-26 14:55:28 +0530

    added the render url to the app.js

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://to-do-app-0ahp.onrender.com/',
 });
 
 export const fetchTodos = () => api.get('/todos').then((r) => r.data);

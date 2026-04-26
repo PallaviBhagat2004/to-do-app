b6b8d97eb5b51c734fc58df136ddcd755e6cb121
PallaviBhagat2004 <bhagatpallavi910@gmail.com>
2026-04-26 14:40:39 +0530

    Update CORS configuration to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const todoRoutes = require('./routes/todoRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
